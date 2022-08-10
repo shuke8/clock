c6b79dca36eaeccd8310919a8dc320ed2cf119ef
Shukrullo <stemirov777@gmail.com>
2022-08-10 14:27:33 +0500

    First commit

---
+++ app.js
@@ -0,0 +1,26 @@
+function displayTime() {
+  let dateTime = new Date();
+  let hrs = dateTime.getHours();
+  let min = dateTime.getMinutes();
+  let sec = dateTime.getSeconds();
+
+  let session = document.getElementById("session");
+
+  session = function () {
+    if (hrs >= 12) {
+      session.innerHTML = "PM";
+    } else {
+      session.innerHTML = "AM";
+    }
+
+    if (hrs > 12) {
+      hrs -= 12;
+    }
+  };
+
+  document.getElementById("hours").innerHTML = hrs;
+  document.getElementById("minutes").innerHTML = min;
+  document.getElementById("seconds").innerHTML = sec;
+}
+
+setInterval(displayTime, 10);
